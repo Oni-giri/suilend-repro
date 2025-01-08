caa0608d81aed1145e5131e472bc2db422e91637
juzybits <96890594+juzybits@users.noreply.github.com>
2025-01-07 23:24:40 -0100

    useAccount() to work with suiprivkey1... format

--- publish/utils/useAccount.ts
+++ publish/utils/useAccount.ts
@@ -1,11 +1,12 @@
+import { decodeSuiPrivateKey } from '@mysten/sui/cryptography';
 import { Ed25519Keypair } from '@mysten/sui/keypairs/ed25519';
 import { config } from 'dotenv';
 
 config(); // Load environment variables from .env file
 
 function useAccount(privateKeyBase64: string): Ed25519Keypair {
-    const privateKey = Buffer.from(privateKeyBase64, 'base64');
-    const keypair = Ed25519Keypair.fromSecretKey(privateKey);
+    const parsedKeypair = decodeSuiPrivateKey(privateKeyBase64);
+    const keypair = Ed25519Keypair.fromSecretKey(parsedKeypair.secretKey);
 
     console.log(`Generated account address: ${keypair.getPublicKey().toSuiAddress()}`);
     return keypair;
